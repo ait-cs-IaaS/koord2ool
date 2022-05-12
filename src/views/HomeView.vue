97772f7b6bbf9c2edcd0a262866876e6acd1a297
Manuel Warum <manuel.warum@ait.ac.at>
2022-05-12 09:57:40 +0200

    Add link to LimeSurvey to landing page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -15,6 +15,16 @@
         <p>More text to come&hellip; for realsies!</p>
       </b-col>
     </b-row>
+    <b-row>
+      <b-col cols="12" md="4" lg="2">
+        <div class="font-weight-bold">Further links:</div>
+      </b-col>
+      <b-col cols="12" md="8" lg="10">
+        <b-link v-if="limesurveyUrl" :href="limesurveyUrl" target="_blank">
+          Limesurvey @ {{ limesurveyUrl }}
+        </b-link>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
@@ -25,6 +35,12 @@ import { Vue, Component } from "vue-property-decorator";
 export default class HomeView extends Vue {
   get isAuthenticated(): boolean {
     return this.$store.getters.isAuthenticated;
+  }
+
+  get limesurveyUrl(): string {
+    const endpoint = process.env.VUE_APP_LIMESURVEY_API;
+    const cutoff = endpoint.lastIndexOf("/admin/remotecontrol");
+    return cutoff !== -1 ? endpoint.substring(0, cutoff) : "";
   }
 }
 </script>
